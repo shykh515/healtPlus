fd190408017fc5713695959e8e83b1c455705652
Parallel Solution <arshad@parallelsolution.com.pk>
2021-12-30 15:47:28 +0500

    First Commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
+import ClinicNavigator from './Navigation/MainNavigator';
+import {Provider} from 'react-redux';
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import AppointmentReducer from './store/reducers/appointmentReducer';
+import AuthReducer from './store/reducers/AuthReducer';
+import PatientReducer from './store/reducers/PatientReducer';
+import ReduxThunk from 'redux-thunk';
+
+
+
+const rootReducer = combineReducers({
+  appointment:AppointmentReducer,
+  auth:AuthReducer,
+  patient:PatientReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk)); 
+export default function App() {
+  return (
+    <Provider store={store}>
+    <View style={styles.screen}>
+    <ClinicNavigator />
+    
+    </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    
+  },
+});
